822fc4428a9d025f9eef19119e8a2b73104cfa93
chondasaine <chonda.saine@gmail.com>
2015-10-04 10:08:30 -0400

    Assignment #3

---
+++ C#/Assignment 3/Assignment 3/Assignment 3/Instructor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    class Instructor
+    {
+        private string FirstName;
+        private string LastName;
+        private string CourseName;
+
+        public Instructor(string FirstName, string LastName,  string CourseName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.CourseName = CourseName;
+        }
+
+        public string SetStudentGrade(string Student, int Grade)
+        {
+            return Student + " " + Grade;
+        }
+        public void PrintInstructorInfor()
+        {
+            string output = this.FirstName;
+            output += " ";
+            output += this.LastName;
+            output += " ";
+            output += this.CourseName;
+            System.Console.WriteLine(output);
+            System.Console.ReadKey();
+
+
+        }
+    }
+
+
+}
---
+++ C#/Assignment 3/Assignment 3/Assignment 3/Student.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Assignment_3
+{
+    class Student
+    {
+        private string StudentFirstName;
+        private string StudentLastName;
+        private int Grade;
+        private Instructor Teacher;
+
+
+        public Student(string StudentFirstName, string StudentLastName, string InstructorTeacher, int Grade)
+        {
+            this.StudentFirstName = StudentFirstName;
+            this.StudentLastName = StudentLastName;
+            this.Teacher = Instructor;
+            this.Grade = 0;
+        }
+        public int SetGrade(int Grade)
+        {
+            return Grade;
+        }
+        public void PrintStudentInfor()
+        {
+            string output = this.StudentFirstName;
+            output += " ";
+            output += this.StudentLastName;
+            output += " ";
+            output += this.Grade;
+            output += " ";
+            output += this.Teacher;
+            System.Console.WriteLine(output);
+            System.Console.ReadKey();
+        }
+}
