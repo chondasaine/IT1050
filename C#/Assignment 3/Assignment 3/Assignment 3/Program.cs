156207b1e87d59e4877b4a3aeeeec778410c10ea
chondasaine <chonda.saine@onbase.com>
2015-10-20 21:29:47 -0400

    Update Program.cs

--- C#/Assignment 3/Assignment 3/Assignment 3/Program.cs
+++ C#/Assignment 3/Assignment 3/Assignment 3/Program.cs
@@ -1,31 +1,31 @@
-﻿namespace Assignment_3
+namespace Assignment3_REV
 {
+
     class Program
-    {
-        static void Main(string[] args)
-        {
+
+    { 
+         static void Main(string[] args)
+         {
             Instructor John = new Instructor("John", "English");
             Instructor Mike = new Instructor("Mike", "Math");
-            Student Jane = new Student("Jane", John , 0);
-            Student Joe = new Student("Joe", John, 0);
-            Student Melissa = new Student("Melissa", Mike, 0);
-            Student Matt = new Student("Matt", Mike, 0);
 
-            Jane.SetGrade(95);
-            Joe.SetGrade(85);
-            Melissa.SetGrade(90);
-            Matt.SetGrade(92);
+            Student Jane = new Student("Jane", John); 
+            Student Joe = new Student("Joe", John); 
+            Student Melissa = new Student("Melissa", Mike); 
+            Student Matt = new Student("Matt", Mike); 
+            
+ 
+            John.SetStudentGrade(Jane, 95 ); 
+            John.SetStudentGrade(Joe, 85); 
+            Mike.SetStudentGrade(Melissa, 90); 
+            Mike.SetStudentGrade(Matt, 92);
 
-            John.SetStudentGrade("Jane", 95 );
-            John.SetStudentGrade("Joe", 85);
-            Mike.SetStudentGrade("Melissa", 90);
-            Mike.SetStudentGrade("Matt", 92);
-
-            Jane.PrintStudentInfor();
-            Joe.PrintStudentInfor();
-            Melissa.PrintStudentInfor();
-            Matt.PrintStudentInfor();
-
-        }
-    }
-}
+            Jane.PrintStudentInfor(); 
+            Joe.PrintStudentInfor(); 
+            Melissa.PrintStudentInfor(); 
+            Matt.PrintStudentInfor(); 
+ 
+ 
+         } 
+     } 
+ } 
